62781b01981fbdee4e4d9487c5218889053e2295
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-01 12:51:31 +0900

    Revert "devise-create"
    This reverts commit 41b97ba9ad0e41a158382a6ea12b7e54b7300314.

--- app/models/address.rb
+++
@@ -1,5 +0,0 @@
-class Address < ApplicationRecord
-  belongs_to :user, optional: true
-  validates :user_id, :postal_code, :prefecture, :city, :block_number, :apartment_name, presence: true
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-  }
   root to: 'products#index'
 end
