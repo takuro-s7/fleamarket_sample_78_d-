2668524bc0d474749ab6a1b2b2d2c022f1e446e4
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-03 14:20:30 +0900

    Revert "Revert "新規登録、ログイン機能追加""
    This reverts commit 4386324b7b49f1c7bec87eb9847c7f77be3dee12.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions:      "users/sessions"
   }
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
+  
   root to: 'products#index'
+  # resources :users, only: :new  
 end
