a8b3ff1e79086d3760571b21e0ccd368d99f7b96
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-03 18:47:29 +0900

    正規表現add

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
-  belongs_to :user, optional: true
+  validates :postal_code, format: {with: (/\A\d{7}\z/)}
+
   validates  :postal_code, :prefecture, :city, :block_number, presence: true
+  belongs_to :user, optional: true
+  
+
 end
