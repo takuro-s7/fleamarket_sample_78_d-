2df06f296688b881232496f6c66d7eab2948d639
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-06 13:11:53 +0900

    新規登録、ログインフロント実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
-  before_action :authenticate_user!
+ 
   
   before_action :basic_auth, if: :production?
-
+  
+ 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
+
+
+  
 
   def production?
     Rails.env.production?
