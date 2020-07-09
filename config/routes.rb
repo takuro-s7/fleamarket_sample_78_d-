850960184a105eebf914a03b3872cf153591d0ac
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-09 17:30:59 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,7 @@ Rails.application.routes.draw do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
   
-  resources :users, only: :new  
-
+  resources :users, only: :new
   resources :products
   get 'products/new/mid_category', to: 'products#mid_category'
   get 'products/new/small_category', to: 'products#small_category'
